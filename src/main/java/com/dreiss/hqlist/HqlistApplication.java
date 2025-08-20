13f514a0410d057ae545b41fec3eea86b5c7830a
Basyan_ <reisdaniel162@gmail.com>
2025-08-20 03:09:07 -0300

    Project created

---
+++ src/main/java/com/dreiss/hqlist/HqlistApplication.java
@@ -0,0 +1,13 @@
+package com.dreiss.hqlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HqlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HqlistApplication.class, args);
+	}
+
+}

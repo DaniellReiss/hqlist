11701fba7b73909a3239ad6b267636906bca3148
Basyan_ <reisdaniel162@gmail.com>
2025-08-20 19:15:18 -0300

    GET hq by id

--- src/main/java/com/dreiss/hqlist/controllers/HqController.java
+++ src/main/java/com/dreiss/hqlist/controllers/HqController.java
@@ -1,10 +1,13 @@
 package com.dreiss.hqlist.controllers;
 
+import com.dreiss.hqlist.dto.HqDTO;
 import com.dreiss.hqlist.dto.HqMinDTO;
 import com.dreiss.hqlist.entities.DC;
 import com.dreiss.hqlist.services.HqService;
+import org.hibernate.annotations.processing.Find;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,11 +20,16 @@ public class HqController {
     @Autowired
     private HqService hqService;
 
+    @GetMapping(value = "/{id}")
+    public HqDTO findById(@PathVariable Long id) {
+        HqDTO result = hqService.findById(id);
+        return result;
+    }
+
     @GetMapping
     public List<HqMinDTO> findAll(){
         List<HqMinDTO> result = hqService.findAll();
         return result;
-
     }
 
 }
--- src/main/java/com/dreiss/hqlist/services/HqService.java
+++ src/main/java/com/dreiss/hqlist/services/HqService.java
@@ -1,8 +1,10 @@
 package com.dreiss.hqlist.services;
 
+import com.dreiss.hqlist.dto.HqDTO;
 import com.dreiss.hqlist.dto.HqMinDTO;
 import com.dreiss.hqlist.entities.DC;
 import com.dreiss.hqlist.repositories.HqRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,14 @@ public class HqService {
     @Autowired
     private HqRepository hqRepository;
 
+    @org.springframework.transaction.annotation.Transactional(readOnly = true)
+    public HqDTO findById(Long id) {
+        DC result = hqRepository.findById(id).get();
+        HqDTO dto = new HqDTO(result);
+        return dto;
+    }
+
+    @org.springframework.transaction.annotation.Transactional(readOnly = true)
     public List<HqMinDTO> findAll() {
         List<DC> result = hqRepository.findAll();
         List<HqMinDTO> dto = result.stream().map(x -> new HqMinDTO(x)).toList();
